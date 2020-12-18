270da20922bd2dab9204cea685d3ac7b384af334
chandanhr-7 <chandanhr7@gmail.com>
2020-12-18 16:42:21 +0530

    Added subs

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,5 +4,9 @@ public class Calc {
 	public void add(int num1,int num2) {
 		System.out.println("Summation="+(num1+num2));
 	}
+	
+	public void subs(int num1,int num2) {
+		System.out.println("Difference="+(num1-num2));
+	}
 
 }
